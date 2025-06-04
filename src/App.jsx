cd02e0b314352b2f0454326f8c9ceca826b2ced6
Miriam Sanders <miriamsanders2005@gmail.com>
2025-06-04 23:15:21 +0300

    fixed css

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,14 @@ import WeatherCard from './components/WeatherCard'
 import './App.css'
 
 function App() {
-  //const cityArray = ["london","eilat","new york","alaska"];
 const cityArray = [
     { name: "london" },
-    { name: "eilat", lat: 29.5581, lon: 34.9482 },
+    {  lat: 29.5581, lon: 34.9482 },
     { name: "new york", lat: 40.7128, lon: -74.0060 },
     { name: "alaska",he_name:"אלסקה" ,lat: 61.3707, lon: -152.4044 }
   ];
   return (
-    <div>
+    <div className='container'>
       <h1>תחזיות מסביב לעולם</h1>
     <div className="grid-container">
       {cityArray.map((cityData, index) =>
