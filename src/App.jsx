0b61d9d65796e0a79362c0f6bcddf6ff4c8aeaad
Miriam Sanders <miriamsanders2005@gmail.com>
2025-06-04 20:03:05 +0300

    added functionality for no long lat

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,17 @@ import './App.css'
 function App() {
   //const cityArray = ["london","eilat","new york","alaska"];
 const cityArray = [
-    { name: "לונדון", lat: 51.5074, lon: -0.1278 },
-    { name: "אילת", lat: 29.5581, lon: 34.9482 },
-    { name: "ניו יורק", lat: 40.7128, lon: -74.0060 },
-    { name: "אלסקה", lat: 61.3707, lon: -152.4044 }
+    { name: "london" },
+    { name: "eilat", lat: 29.5581, lon: 34.9482 },
+    { name: "new york", lat: 40.7128, lon: -74.0060 },
+    { name: "alaska",he_name:"אלסקה" ,lat: 61.3707, lon: -152.4044 }
   ];
   return (
     <div>
       <h1>תחזיות מסביב לעולם</h1>
     <div className="grid-container">
       {cityArray.map((cityData, index) =>
-        <WeatherCard key={index} city={cityData.name}  lat={cityData.lat} lon={cityData.lon}/>
+        <WeatherCard key={index}   cityData={cityData} />
       )}
     </div>
     </div>
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -2,15 +2,27 @@ import React, { useEffect } from "react";
 import { Sun, CloudHail, CloudSun } from "lucide-react"
 import "../styles/WeatherCard.css"; // Adjust the path as necessary
 
-const WeatherCard = ({ city, lat, lon }) => {
+const WeatherCard = ({ cityData }) => {
     const [weatherData, setWeatherData] = React.useState(null);
-
     const fetchWeatherData = async () => {
         try {
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_WEATHER_API_KEY}&units=metric&lang=he`);
-            const data = await response.json();
-            setWeatherData(data);
+            // instead of using the city name, we can use the lat and lon props to fetch the weather data. (if we were to add an option of manually adding a city, we would need to use the city name to get the lat and lon)
+            let weatherResponse;
+            if (cityData.lat || !cityData.lon) {
+                const cityResponse = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityData.name}&appid=${import.meta.env.VITE_WEATHER_API_KEY}`);
+                const cityDataResponse = await cityResponse.json();
+                weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${cityDataResponse[0].lat}&lon=${cityDataResponse[0].lon}&appid=${import.meta.env.VITE_WEATHER_API_KEY}&units=metric&lang=he`);
+            }
+            else {
+                weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${cityData.lat}&lon=${cityData.lon}&appid=${import.meta.env.VITE_WEATHER_API_KEY}&units=metric&lang=he`);
+            }
+            const data = await weatherResponse.json();
             console.log("Weather data:", data);
+
+            if (weatherResponse.status !== 200) {
+                throw new Error(data.message || "Failed to fetch weather data");
+            }
+            setWeatherData(data);
         } catch (error) {
             console.error("Error fetching weather data:", error);
             return null;
@@ -18,17 +30,17 @@ const WeatherCard = ({ city, lat, lon }) => {
     }
     useEffect(() => {
         fetchWeatherData();
-    }, [city, lat, lon]);
+    }, [cityData]);
     if (!weatherData) {
         return <div className="weather-card">Loading...</div>;
     }
     const getWeatherIcon = (temp) => {
         if (temp <= 20) {
-            return <CloudHail color="blue" fill="blue" size="2rem"/>;
+            return <CloudHail color="#4A90E2" fill="#4A90E2" size="2.5rem" />;
         } else if (temp >= 30) {
-            return <Sun color="rgba(243, 188, 9, 0.87)"  fill="rgba(243, 188, 9, 0.87)" size="2rem"/>;
+            return <Sun color="#F3BC09" fill="#F3BC09" size="2.5rem" />;
         } else {
-            return <CloudSun color="green" fill="green" size="2rem"/>;
+            return <CloudSun color="#6BAF92" fill="#6BAF92" size="2.5rem" />;
         }
     };
 
@@ -37,7 +49,7 @@ const WeatherCard = ({ city, lat, lon }) => {
             <div className="weather-header">
                 <div className="weather-icon">{getWeatherIcon(weatherData.main.temp)}</div>
                 <div className="location-container">
-                    <h2 className="city-name">{city}</h2>
+                    <h2 className="city-name">{cityData.he_name || weatherData.name}</h2>
                     <p className="weather-description">{weatherData.weather[0].description}</p>
                 </div>
             </div>
