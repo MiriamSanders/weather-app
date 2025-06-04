95a2ed858076714d33539364fc5684abcc122735
Miriam Sanders <miriamsanders2005@gmail.com>
2025-06-04 19:11:14 +0300

    added fetch to retrive weater data and a function to display the correct icon

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,19 @@ import WeatherCard from './components/WeatherCard'
 import './App.css'
 
 function App() {
-  const cityArray = ["london","eilat","new york","alaska"];
+  //const cityArray = ["london","eilat","new york","alaska"];
+const cityArray = [
+    { name: "לונדון", lat: 51.5074, lon: -0.1278 },
+    { name: "אילת", lat: 29.5581, lon: 34.9482 },
+    { name: "ניו יורק", lat: 40.7128, lon: -74.0060 },
+    { name: "אלסקה", lat: 61.3707, lon: -152.4044 }
+  ];
   return (
     <div>
       <h1>תחזיות מסביב לעולם</h1>
     <div className="grid-container">
-      {cityArray.map((city, index) =>
-        <WeatherCard key={index} city={city} />
+      {cityArray.map((cityData, index) =>
+        <WeatherCard key={index} city={cityData.name}  lat={cityData.lat} lon={cityData.lon}/>
       )}
     </div>
     </div>
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,41 +1,58 @@
-import React from "react";
-import { Sun } from "lucide-react"
+import React, { useEffect } from "react";
+import { Sun, CloudHail, CloudSun } from "lucide-react"
 import "../styles/WeatherCard.css"; // Adjust the path as necessary
 
-const WeatherCard = ({ city }) => {
-    const fetchWeatherData = async (city) => {
+const WeatherCard = ({ city, lat, lon }) => {
+    const [weatherData, setWeatherData] = React.useState(null);
+
+    const fetchWeatherData = async () => {
         try {
-            const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=YOUR_API_KEY&q=${city}`);
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_WEATHER_API_KEY}&units=metric&lang=he`);
             const data = await response.json();
-            return data;
+            setWeatherData(data);
+            console.log("Weather data:", data);
         } catch (error) {
             console.error("Error fetching weather data:", error);
             return null;
         }
     }
-
+    useEffect(() => {
+        fetchWeatherData();
+    }, [city, lat, lon]);
+    if (!weatherData) {
+        return <div className="weather-card">Loading...</div>;
+    }
+    const getWeatherIcon = (temp) => {
+        if (temp <= 20) {
+            return <CloudHail color="blue" fill="blue" size="2rem"/>;
+        } else if (temp >= 30) {
+            return <Sun color="rgba(243, 188, 9, 0.87)"  fill="rgba(243, 188, 9, 0.87)" size="2rem"/>;
+        } else {
+            return <CloudSun color="green" fill="green" size="2rem"/>;
+        }
+    };
 
     return (
         <div className="weather-card">
             <div className="weather-header">
-                <div className="sun-icon"><Sun color=" rgba(243, 188, 9, 0.87)" /></div>
+                <div className="weather-icon">{getWeatherIcon(weatherData.main.temp)}</div>
                 <div className="location-container">
-                    <h2 className="city-name">לונדון</h2>
-                    <p className="weather-description">שבירי עננים</p>
+                    <h2 className="city-name">{city}</h2>
+                    <p className="weather-description">{weatherData.weather[0].description}</p>
                 </div>
             </div>
             <div className="weather-grid">
                 <div className="weather-item">
                     <p className="weather-label">לחות</p>
-                    <p className="weather-value">52%</p>
+                    <p className="weather-value">{weatherData.main.humidity}%</p>
                 </div>
                 <div className="weather-item">
-                    <p className="weather-label">טמפ' מרבית</p>
-                    <p className="weather-value">30°C</p>
+                    <p className="weather-label">טמפ' נמדדת</p>
+                    <p className="weather-value">{weatherData.main.temp}</p>
                 </div>
                 <div className="weather-item">
-                    <p className="weather-label">טמפ' מזערית</p>
-                    <p className="weather-value">29°C</p>
+                    <p className="weather-label">טמפ' מורגשת</p>
+                    <p className="weather-value">{weatherData.main.feels_like}</p>
                 </div>
             </div>
         </div>
